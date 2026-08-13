d6434c32adaee7d68fbac01699f4b824f3910297
sukhveeeer <151768037+sukhveeeer@users.noreply.github.com>
2026-08-13 12:11:09 +0530

    UPDATE2

---
+++ js5.js
@@ -0,0 +1,34 @@
+// 1. Write a loop to print numbers from 1 to 10.\
+let i=0
+for( i=1 ;i <= 10 ;i++)
+{
+    console.log(i);
+}
+// 2 Write a program to print all natural numbers in reverse (from 10 to 1).
+for(i=10; i >=1; i--){
+    console.log(i)
+}
+
+// 3. Write a loop to print all even numbers between 1 and 20.
+for(i=2; i<=20; i+=2){
+    console.log(i)
+}
+// 4. Write a loop to calculate the sum of numbers from 1 to 100.
+let sum =0;
+for(i=1;i<=100;i++){
+    sum=sum+i;
+    console.log(sum)
+// }
+// 5. Write a program to find sum of all odd numbers between 1 to 25.
+// let ashwin=0;
+// for(vijay=1;vijay<=25;vijay+=2){
+//     ashwin=ashwin+vijay;
+
+// }
+// console.log(ashwin)
+// 6. Write a loop to print an array in reverse order. Example: [1, 2, 3, 4, 5] should print 5, 4, 3, 2, 1.
+
+
+
+
+
