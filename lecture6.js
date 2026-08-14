3cbcfebc5cf62cd7ed7a0b0a23b7115a221f5549
sukhveeeer <151768037+sukhveeeer@users.noreply.github.com>
2026-08-14 12:36:31 +0530

    update3

---
+++ lecture6.js
@@ -0,0 +1,37 @@
+//7. Write a loop to print the multiplication table of 5.
+
+// for(b=6; b <=60;b+=6){
+    
+//     console.log(b)
+//  }
+//  let a=7;
+//  for(i=1;i<=10;i++){
+//     var c=a*i;
+//     console.log(c)
+//  }
+
+// 8. Write a loop to calculate the factorial of a number (e.g., 5! = 120)
+// let a =5;
+// for(let i=a-1;i>=1;i--){
+//     a=a*i;
+
+// }
+//     console.log(a);
+// 6. Write a loop to print an array in reverse order. Example: [1, 2, 3, 4, 5] should print 5, 4, 3, 2, 1.
+// let arr=[1,2,3,4,5]
+// for(let b=arr.length-1;b>=0;b--){
+// console.log(arr[b])
+// }
+// 9. Write a loop to count the number of times 3 appears in [1, 3, 5, 3, 3, 7].
+
+let arr=[1,3,5,3,3,7]
+var b=0;
+for(let c=0;c< arr.length;c++){
+    if(arr [c]==3){
+        // console.log("3 is repeated ")
+        b++;
+    }   
+}
+// console.log(b)
+// 10. Write a program to find sum of all prime numbers between 1 to 100.
+
